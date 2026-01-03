fd68fa1c69986808de41ecd15d5ed853a912aa91
meriembouchenine <232333309919@univ-jijel.dz>
2026-01-03 23:15:48 +0100

    add

--- MyGitApp/src/main/java/com/l3/gl/model/User.java
+++ MyGitApp/src/main/java/com/l3/gl/model/User.java
@@ -1,10 +1,52 @@
 package com.l3.gl.model;
 
-public class User {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class User{
+    private List<Product> tasks = new ArrayList<>();
+    private final String FILE = "tasks.txt";
 
-	}
+    public User() {
+        load();
+    }
 
+    public void addTask(Product task) {
+        tasks.add(task);
+        save();
+    }
+
+    public void removeTask(Product task) {
+        tasks.remove(task);
+        save();
+    }
+
+    public List<Product> getTasks() {
+        return tasks;
+    }
+
+    private void save() {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(FILE))) {
+            for (Product t : tasks) {
+                pw.println(t.toFileString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void load() {
+        File f = new File(FILE);
+        if (!f.exists()) return;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                tasks.add(Product.fromFileString(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
